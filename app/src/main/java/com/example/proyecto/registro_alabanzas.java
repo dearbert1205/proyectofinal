941004489a3faa0498987f8e0c0d80de0daac5da
jessicamelgar <jessicamelgar04@gmail.com>
2019-11-20 11:51:45 -0600

    38commit

--- app/src/main/java/com/example/proyecto/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto/registro_alabanzas.java
@@ -85,7 +85,9 @@ public class registro_alabanzas extends AppCompatActivity {
 
             buscar.addTextChangedListener(new TextWatcher() {
                 @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+                }
 
                 @Override
                 public void onTextChanged(CharSequence s, int start, int before, int count) {
