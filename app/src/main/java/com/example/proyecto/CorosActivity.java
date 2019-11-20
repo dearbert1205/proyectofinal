d7adc858593c9a3852172c619540fba46c79332b
estefanyca <kathyflores606@gmail.com>
2019-11-20 12:04:09 -0600

    46commit

--- app/src/main/java/com/example/proyecto/CorosActivity.java
+++ app/src/main/java/com/example/proyecto/CorosActivity.java
@@ -29,7 +29,9 @@ public class CorosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coros);
 
-
+        ettituloc = findViewById(R.id.ettituloc);
+        etautorc = findViewById(R.id.etautorc);
+        etletrac = findViewById(R.id.etletrac);
 
         btnRegistrarc = findViewById(R.id.btnRegistrarC);
 
