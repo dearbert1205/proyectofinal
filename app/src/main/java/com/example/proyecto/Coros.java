082224e66100ca28ba5ab50ec685b735060e8cf6
estefanyca <kathyflores606@gmail.com>
2019-11-20 12:08:52 -0600

    53commit

--- app/src/main/java/com/example/proyecto/Coros.java
+++ app/src/main/java/com/example/proyecto/Coros.java
@@ -1,10 +1,6 @@
 package com.example.proyecto;
 
-public class Coros {
-    private int id;
-    private String titulo;
-    private String autor;
-    private String letra;
+
 
     public Coros() {
     }
