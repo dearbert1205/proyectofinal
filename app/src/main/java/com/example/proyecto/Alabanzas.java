046bdc8ee654edf9e3625985c00cb74806f9ffe3
stefany-96 <stefanyghernandezh1@gmail.com>
2019-11-20 13:37:39 -0600

    78commit

--- app/src/main/java/com/example/proyecto/Alabanzas.java
+++ app/src/main/java/com/example/proyecto/Alabanzas.java
@@ -1,6 +1,10 @@
 package com.example.proyecto;
 
-
+public class Alabanzas {
+    private int id;
+    private String titulo;
+    private String autor;
+    private String letra;
 
     public Alabanzas() {
     }
